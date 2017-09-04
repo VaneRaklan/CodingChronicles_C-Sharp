6b01f261397b486f34b25fc5b711d4fe6984310c
Antonio Ramirez <vaneraklan@D-10-157-23-164.dhcp4.washington.edu>
2017-09-04 10:05:37 -0700

    Added info about variable types and the bounds of numeric variable types

---
+++ CSharpVol1.cs
@@ -0,0 +1,47 @@
+using System;
+
+class CSharpVol1
+{
+    static void Main(String[] args)
+    {
+
+    }
+
+    /* //Creating Variables
+    There are several variable data types, but the primary ones are:
+    -int        (A whole number)
+    -double     (A number with a decimal place or extends out of integer boundaries)
+    -String     (Written characters; name, phrase, etc.)
+    -Char       (an individual character)
+    -boolean    (a value of true or false)
+    -Object     (we will get to Objects in a different section)
+
+    Each of the numeric data types has their own boundaries:
+    -byte       =   0 to 255
+    -sbyte      =   -128 to 127
+    -short      =   -32,768 to 32,767
+    -ushort     =   0 to 65,535
+    -int        =   -2,147,483,648 to 2,147,483,647
+    -uint       =   0 to 4,294,967,295
+    -long       =   
+    -ulong      =   
+    -float      =   
+    -double     =   
+    -decimal    =   
+    
+    Knowing how much memory is being alloted for what variables
+        doesn't necessarily take a toll within small projects,
+        but on a massive scale, allotting more memory to hold a
+        variable that doesn't use it all will eventually depreciate
+        the efficiency of the program
+    */
+
+
+    /* //Hello World && Basic Print statement
+    //The first example for any beginning programmer
+        Console.WriteLine("Hello World");
+
+    Console.WriteLine(); is how to print a 
+    statement to the console/terminal
+    */
+}
